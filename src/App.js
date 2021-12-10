07956b1755ed8f7548247ae73626b52f11810212
khalid <dev.khalidhossain@gmail.com>
2021-12-11 02:33:01 +0600

    "Conditional rendering added"

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Clock from "./Components/Clock";
 function App() {
-  return <Clock local="bn-BD" />;
+  return <Clock />;
 }
 
 export default App;
--- src/Components/Clock.js
+++ src/Components/Clock.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 export default class Clock extends Component {
   state = {
     date: new Date(),
+    local: "bn-BD",
+    btn: true,
   };
 
   componentDidMount() {
@@ -13,6 +15,20 @@ export default class Clock extends Component {
     clearInterval(this.timer);
   }
 
+  // bnClick = () => {
+  //   this.setState({ local: "bn-BD" });
+  //   console.log("clicked");
+  // };
+  // enClick = () => {
+  //   this.setState({ local: "en-US" });
+  //   console.log("clicked");
+  // };
+
+  onClick = (value) => {
+    this.setState({ local: value });
+    // console.log("clicked");
+  };
+
   tick() {
     this.setState({
       date: new Date(),
@@ -20,18 +36,29 @@ export default class Clock extends Component {
   }
 
   style = {
-    margin: " auto 40%",
-    backgroundColor: "yellow",
-    color: "black",
-    border: "1px solid black",
+    // margin: " auto 40%",
+    // backgroundColor: "grey",
+    // maxHeight: "100vh",
+    // color: "black",
+    // border: "1px solid black",
     textAlign: "center",
   };
-  render() {
+  render = () => {
     return (
       <div style={this.style}>
         <p>Local time</p>
-        <h1>{this.state.date.toLocaleTimeString(this.props.local)}</h1>
+        <h1>{this.state.date.toLocaleTimeString(this.state.local)}</h1>
+        {this.state.local === "en-US" ? (
+          <button onClick={()=>this.onClick("bn-BD")}>
+            {this.state.btn && "Translate to Bangla"}
+          </button>
+        ) : (
+         /*  {<button onClick={() => this.setState({ local: "en-US" })}> } */
+         <button onClick={()=>this.onClick("en-US")}>
+            {this.state.btn && "Translate to English"}
+          </button>
+        )}
       </div>
     );
-  }
+  };
 }
